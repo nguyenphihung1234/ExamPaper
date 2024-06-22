2e379fdd63a5e8c71e6194c28075c739ab39afba
nguyenphihung1234 <nguyenphihung23121983@gmail.com>
2024-06-22 14:50:34 +0700

    Initial commit

---
+++ model/Contact.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamPaper.model
+{
+    public class Contact
+    {
+        
+    }
+}
